5fd1c3a8cf4c78cb3f2ceb337f26b85c1559c3a7
Lise <lise692@gmail.com>
2024-01-14 16:43:56 +0100

    add and export function for unique id

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const favicon = require('serve-favicon');
-const { success } = require('./helper');
+const { success, getUniqueId } = require('./helper');
 const pokemons = require('./mock-pokemon');
 
 const app = express();
