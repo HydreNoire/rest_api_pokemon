e5a8c610af466c235184155e0c342719b9d00e54
Lise <lise692@gmail.com>
2024-01-14 21:10:36 +0100

    add sequelize + create new sequelize class

--- app.js
+++ app.js
@@ -2,11 +2,28 @@ const express = require('express');
 const morgan = require('morgan');
 const favicon = require('serve-favicon');
 const bodyParser = require('body-parser');
+const { Sequelize } = require('sequelize');
 const { success, getUniqueId } = require('./helper');
 let pokemons = require('./mock-pokemon');
 
 const app = express();
 const port = 3000;
+
+const sequelize = new Sequelize(
+    'pokedex',
+    'root',
+    '',
+    {
+        host: 'localhost',
+        dialect: 'mariadb',
+        dialectOptions: {
+            timezone: 'Etc/GMT-2'
+        },
+        logging: false
+    }
+);
+
+
 
 app
     .use(favicon(__dirname + '/favicon.ico'))
@@ -44,6 +61,14 @@ app.put('/api/pokemons/:id', (req, res) => {
     });
     const message = `Le pokemon ${pokemonUpdated.name} a bien été modifié`;
     res.json(success(message, pokemonUpdated));
+})
+
+app.delete('/api/pokemons/:id', (req, res) => {
+    const pokemonId = parseInt(req.params.id);
+    const pokemonDeleted = pokemons.find(pokemon => pokemon.id === pokemonId);
+    pokemons = pokemons.filter(pokemon => pokemon.id !== pokemonId);
+    const message = `Le pokemon ${pokemonDeleted.name} a bien été supprimé.`
+    res.json(success(message, pokemonDeleted));
 })
 
 app.listen(port, () => console.log(`Notre application Node démarre sur : http://localhost:${port}`));
