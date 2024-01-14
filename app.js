10d496c988eae82df296b12fe75eee95514aba83
Lise <lise692@gmail.com>
2024-01-14 22:43:51 +0100

    add put and delete endpoints

--- app.js
+++ app.js
@@ -18,5 +18,7 @@ sequelize.initDB();
 require('./src/routes/findAllPokemons')(app)
 require('./src/routes/findPokemonByPk')(app)
 require('./src/routes/createPokemon')(app)
+require('./src/routes/updatePokemon')(app)
+require('./src/routes/deletePokemon')(app)
 
 app.listen(port, () => console.log(`Notre application Node démarre sur : http://localhost:${port}`));
