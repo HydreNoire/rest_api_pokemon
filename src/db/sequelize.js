0e1231932c43ce568924d66fd370ebf4058d9390
Lise <lise692@gmail.com>
2024-01-17 21:55:27 +0100

    add bcrypt for password storage

--- src/db/sequelize.js
+++ src/db/sequelize.js
@@ -1,6 +1,8 @@
 const { Sequelize, DataTypes } = require('sequelize');
 const PokemonModel = require('../models/pokemon');
 let pokemons = require('./mock-pokemon');
+const UserModel = require('../models/user');
+const bcrypt = require('bcrypt');
 
 const sequelize = new Sequelize(
     'pokedex',
@@ -17,22 +19,26 @@ const sequelize = new Sequelize(
 );
 
 const Pokemon = PokemonModel(sequelize, DataTypes);
+const User = UserModel(sequelize, DataTypes);
 
 const initDB = () => {
-    return sequelize.sync({ force: true })
-        .then(_ => {
-            pokemons.map(pokemon => {
-                Pokemon.create({
-                    name: pokemon.name,
-                    hp: pokemon.hp,
-                    cp: pokemon.cp,
-                    picture: pokemon.picture,
-                    types: pokemon.types
-                })
-                // .then(pokemon => console.log(pokemon.toJSON()))
-            })
-            console.log('La base de donnée à bien été initialisée')
+    return sequelize.sync({ force: true }).then(_ => {
+        pokemons.map(pokemon => {
+            Pokemon.create({
+                name: pokemon.name,
+                hp: pokemon.hp,
+                cp: pokemon.cp,
+                picture: pokemon.picture,
+                types: pokemon.types
+            })//.then(pokemon => console.log(pokemon.toJSON()))
         })
+
+        bcrypt.hash('pikachou', 10)
+            .then(hash => User.create({ username: "pikachou", password: hash }))
+            .then(user => console.log(user.toJSON()))
+
+        console.log('La base de donnée à bien été initialisée')
+    })
 }
 
 module.exports = {
