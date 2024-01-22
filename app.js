360c9ff28832e1482d7f91f490816d2233caf45f
Lise <lise692@gmail.com>
2024-01-22 20:14:00 +0100

    add cors package

--- app.js
+++ app.js
@@ -10,12 +10,13 @@ const port = process.env.PORT || 3000;
 app
     .use(favicon(__dirname + '/favicon.ico'))
     .use(bodyParser.json())
-    .use((req, res, next) => {
-        res.setHeader('Access-Control-Allow-Origin', '*');
-        res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-        next();
-    })
+    .use(cors())
+// .use((req, res, next) => {
+//     res.setHeader('Access-Control-Allow-Origin', '*');
+//     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+//     next();
+// })
 
 sequelize.initDB();
 
